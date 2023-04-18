e1e776be4eeeae73b361ada9129857a110677fa8
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-04-19 01:16:43 +0300

    add changes in data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ import { GlobalStyle } from "./GlobalStyle";
 import { Profile } from "./profile/Profile";
 import user from './profile/user.json'
 import { Layout } from "./Layout/Layout";
-import { Statistics } from "./Statistics/Statistics";
-import data from './Statistics/data.json'
+// import { Statistics } from "./Statistics/Statistics";
+// import data from './Statistics/data.json'
 
 export const App = () => {
   return (
     <Layout>
       <Profile items={user} />
       <GlobalStyle />
-      <Statistics items={data} />
+      {/* <Statistics items={data} /> */}
     </Layout>
   );
 };
