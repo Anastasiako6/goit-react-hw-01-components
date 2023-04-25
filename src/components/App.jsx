662219bb213da98850ff3954aa3e6b20cd280881
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-04-25 23:30:01 +0300

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ import { GlobalStyle } from "./GlobalStyle";
 import { Layout } from "./Layout/Layout";
 
 import { Profile } from "./Profile/Profile";
-import user from './Profile/user.json'
+import user from './Profile/user.json';
 
 import { Statistics } from "./Statistics/Statistics";
-import data from './Statistics/data.json'
+import data from './Statistics/data.json';
 
 import { FriendList } from './FriendList/FriendList'
-import friends from './FriendList/friends.json'
+import friends from './FriendList/friends.json';
 
-import { TransactionHistory } from './Transaction/TransactionHistory'
+import { TransactionHistory } from './Transaction/TransactionHistory';
 import transactions  from "./Transaction/transactions.json";
 
 export const App = () => {
--- src/components/profile/UserName.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const UserName = styled.p`
-    font-size: x-large;
-    margin-bottom: 15px;
-    margin-top: 25px;
-    
-`
