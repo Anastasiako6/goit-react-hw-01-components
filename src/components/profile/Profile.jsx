8d9641ceb7b7c0769158f1022e1cb21e98b3304e
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-04-25 23:37:01 +0300

    changes

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,64 +1,64 @@
-import PropTypes from 'prop-types';
-import {
-  UserProf,
-  InfoCard,
-  StatsList,
-  StatisItem,
-  PhotoCard,
-  UserName,
-  ParInfo,
-  StatisSpan,
-  SpanNumbers,
-  InfoTag,
-} from './Profile.styled';
+// import PropTypes from 'prop-types';
+// import {
+//   UserProf,
+//   InfoCard,
+//   StatsList,
+//   StatisItem,
+//   PhotoCard,
+//   UserName,
+//   ParInfo,
+//   StatisSpan,
+//   SpanNumbers,
+//   InfoTag,
+// } from './Profile.styled';
 
 
-export const Profile = ({ 
-  items: { username, tag, location, avatar, stats: {
-    followers, views, likes
-  }},
- }) => {
-  return (
-      <UserProf>
-  <InfoCard>
-    <PhotoCard
-      src={avatar}
-      alt={username}
-      width="240px"
-    />
-    <UserName>{username}</UserName>
-    <InfoTag>@{tag}</InfoTag>
-    <ParInfo>{location}</ParInfo>
-  </InfoCard>
+// export const Profile = ({ 
+//   items: { username, tag, location, avatar, stats: {
+//     followers, views, likes
+//   }},
+//  }) => {
+//   return (
+//       <UserProf>
+//   <InfoCard>
+//     <PhotoCard
+//       src={avatar}
+//       alt={username}
+//       width="240px"
+//     />
+//     <UserName>{username}</UserName>
+//     <InfoTag>@{tag}</InfoTag>
+//     <ParInfo>{location}</ParInfo>
+//   </InfoCard>
 
-  <StatsList>
-    <StatisItem>
-      <StatisSpan>Followers</StatisSpan>
-      <SpanNumbers>{followers}</SpanNumbers>
-    </StatisItem>
-    <StatisItem>
-      <StatisSpan>Views</StatisSpan>
-      <SpanNumbers>{views}</SpanNumbers>
-    </StatisItem>
-    <StatisItem>
-      <StatisSpan>Likes</StatisSpan>
-      <SpanNumbers>{likes}</SpanNumbers>
-    </StatisItem>
-  </StatsList>
-    </UserProf>
-  )
-}
+//   <StatsList>
+//     <StatisItem>
+//       <StatisSpan>Followers</StatisSpan>
+//       <SpanNumbers>{followers}</SpanNumbers>
+//     </StatisItem>
+//     <StatisItem>
+//       <StatisSpan>Views</StatisSpan>
+//       <SpanNumbers>{views}</SpanNumbers>
+//     </StatisItem>
+//     <StatisItem>
+//       <StatisSpan>Likes</StatisSpan>
+//       <SpanNumbers>{likes}</SpanNumbers>
+//     </StatisItem>
+//   </StatsList>
+//     </UserProf>
+//   )
+// }
 
-Profile.propTypes = {
-  items: PropTypes.shape({
-    avatar: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    stats: PropTypes.shape({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-    }).isRequired,
-  }).isRequired,
-};
+// Profile.propTypes = {
+//   items: PropTypes.shape({
+//     avatar: PropTypes.string.isRequired,
+//     username: PropTypes.string.isRequired,
+//     tag: PropTypes.string.isRequired,
+//     location: PropTypes.string.isRequired,
+//     stats: PropTypes.shape({
+//       followers: PropTypes.number.isRequired,
+//       views: PropTypes.number.isRequired,
+//       likes: PropTypes.number.isRequired,
+//     }).isRequired,
+//   }).isRequired,
+// };
--- src/components/profile/Profile.styled.jsx
+++ src/components/profile/Profile.styled.jsx
@@ -1,69 +1,69 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const UserProf = styled.div`
-    display: flex;
-    flex-direction: column;
+// export const UserProf = styled.div`
+//     display: flex;
+//     flex-direction: column;
 
-    align-items: center;
-    text-align: center;
-    padding: 50px 70px;
-`;
+//     align-items: center;
+//     text-align: center;
+//     padding: 50px 70px;
+// `;
 
-export const SpanNumbers = styled.span`
-    color: ${p => p.theme.colors.text};
-    font-size: x-large;
-`;
+// export const SpanNumbers = styled.span`
+//     color: ${p => p.theme.colors.text};
+//     font-size: x-large;
+// `;
 
-export const ParInfo = styled.p`
-    color: #b7afaf;
-`;
+// export const ParInfo = styled.p`
+//     color: #b7afaf;
+// `;
 
-export const StatisItem = styled.li`
-    padding: 15px;
+// export const StatisItem = styled.li`
+//     padding: 15px;
 
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
+//     display: flex;
+//     flex-direction: column;
+//     gap: 10px;
 
-    width: 100%;
-    border: 1px solid #afb1b8;
-    text-align: center;
-`;
+//     width: 100%;
+//     border: 1px solid #afb1b8;
+//     text-align: center;
+// `;
 
-export const InfoCard = styled.div`
-    padding: 25px 50px;
-    background-color: ${p => p.theme.colors.bgCard};
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-    font-family: sans-serif;
-    width: auto;
-`;
+// export const InfoCard = styled.div`
+//     padding: 25px 50px;
+//     background-color: ${p => p.theme.colors.bgCard};
+//     border-top-right-radius: 5px;
+//     border-top-left-radius: 5px;
+//     font-family: sans-serif;
+//     width: auto;
+// `;
 
-export const InfoTag = styled.span`
-    display: block;
-    margin-bottom: 15px;
-    color: #b7afaf;
-`;
+// export const InfoTag = styled.span`
+//     display: block;
+//     margin-bottom: 15px;
+//     color: #b7afaf;
+// `;
 
-export const PhotoCard = styled.img`
-    align-self: center;
-    width: 150px;
-    border-radius: 50%;
-`;
-export const StatisSpan = styled.span`
-    font-size: 16px;
-    font-family: initial;
-    color: #b7b3b3;
-`;
+// export const PhotoCard = styled.img`
+//     align-self: center;
+//     width: 150px;
+//     border-radius: 50%;
+// `;
+// export const StatisSpan = styled.span`
+//     font-size: 16px;
+//     font-family: initial;
+//     color: #b7b3b3;
+// `;
 
-export const StatsList = styled.ul`
-    display: flex;
-    background-color: #f3f0f0;
-`;
+// export const StatsList = styled.ul`
+//     display: flex;
+//     background-color: #f3f0f0;
+// `;
 
-export const UserName = styled.p`
-    font-size: x-large;
-    margin-bottom: 15px;
-    margin-top: 25px;
+// export const UserName = styled.p`
+//     font-size: x-large;
+//     margin-bottom: 15px;
+//     margin-top: 25px;
     
-`
+// `
