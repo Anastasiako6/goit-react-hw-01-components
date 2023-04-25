3c44e88570638eece953ed62ce9b70c539b57380
Anastasiako6 <anastasiakorotenko3@gmail.com>
2023-04-25 23:36:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { GlobalStyle } from "./GlobalStyle";
 import { Layout } from "./Layout/Layout";
 
-import { Profile } from "./profile/Profile";
-import user from './profile/user.json';
+// import { Profile } from "./Profile/Profile";
+// import user from './Profile/user.json';
 
 import { Statistics } from "./Statistics/Statistics";
 import data from './Statistics/data.json';
@@ -16,9 +16,9 @@ import transactions  from "./Transaction/transactions.json";
 export const App = () => {
   return (
     <Layout>
-      <Profile items={user} />
-      <GlobalStyle />
-      <Statistics title="Upload stats" stats={data} />
+      {/* <Profile items={user} />; */}
+      <GlobalStyle />;
+      <Statistics title="Upload stats" stats={data} />;
       <FriendList friends={friends} />;
       <TransactionHistory items={transactions} />;
     </Layout>
